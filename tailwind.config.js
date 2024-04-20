3dc9ad8d796698acc67b165f9f2df65e502d1f5d
salvo <you@example.com>
2024-04-19 16:26:24 -1200

    product

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: "rgb(118, 171, 174)",
+      secodary: "rgb(34, 40, 49)",
+      txt: "rgb(238, 238, 238)"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
